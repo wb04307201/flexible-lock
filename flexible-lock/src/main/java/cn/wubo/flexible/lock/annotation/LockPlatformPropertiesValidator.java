1ac29924e62a68a51d09b736f881d8003c465d7f
吴博 <wubo_aaa@163.com>
2025-09-02 23:23:21 +0800

    feat(flexible-lock): 优化锁平台属性验证注解
    -为 LockPlatformPropertiesValidator 注解添加默认消息、分组和负载属性
    - 这些新增的属性使得注解在使用时更加灵活，能够更好地与其他验证框架集成

--- flexible-lock/src/main/java/cn/wubo/flexible/lock/annotation/LockPlatformPropertiesValidator.java
+++ flexible-lock/src/main/java/cn/wubo/flexible/lock/annotation/LockPlatformPropertiesValidator.java
@@ -4,6 +4,7 @@ import cn.wubo.flexible.lock.propertes.LockPlatformProperties;
 import jakarta.validation.Constraint;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -18,6 +19,12 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = LockPlatformPropertiesValidator.Validator.class)
 @Documented
 public @interface LockPlatformPropertiesValidator {
+    String message() default "Invalid lock platform properties";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
     class Validator implements ConstraintValidator<LockPlatformPropertiesValidator, LockPlatformProperties> {
         @Override
         public boolean isValid(LockPlatformProperties properties, ConstraintValidatorContext context) {
